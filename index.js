845fa0b3aaabe9661840e8880c012e2e92578c39
Kishor Bhagat <kishorebhagat663@gmail.com>
2023-02-07 22:11:02 +0530

    cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const connectDB = require('./db');
 const app = express();
 const authRoute = require('./routes/auth');
-// const cors = require('cors');
+const cors = require('cors');
 
 connectDB();
 
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-// app.use(cors());
+app.use(cors());
 app.use('/api/loc', authRoute);
 
 app.listen(port, ()=>{
